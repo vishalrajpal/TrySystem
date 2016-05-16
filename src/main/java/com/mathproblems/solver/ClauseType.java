8186beeae7b85e569715f56457b8bd19640a6341
Vishal Rajpal <vishalrajpal.apiit@gmail.com>
2016-05-15 23:55:55 -0700

    associating verb type with clause type.

--- src/main/java/com/mathproblems/solver/ClauseType.java
+++ src/main/java/com/mathproblems/solver/ClauseType.java
@@ -2,14 +2,22 @@ package com.mathproblems.solver;
 
 public enum ClauseType {
 
-    SV,
-    SVC,
-    SVA,
-    SVO,
-    SVOO,
-    SVOC,
-    SVOA,
-    EXISTENTIAL,
-    UNKNOWN;
+    SV(VerbType.INTRANSITIVE),
+    SVC(VerbType.COPULAR),
+    SVA(VerbType.EXTENDED_COPULAR),
+    SVO(VerbType.MONOTRANSITIVE),
+    SVOO(VerbType.DITRANSITIVE),
+    SVOC(VerbType.COMPLEX_TRANSITIVE),
+    SVOA(VerbType.COMPLEX_TRANSITIVE),
+    EXISTENTIAL(VerbType.UNKNOWN),
+    UNKNOWN(VerbType.UNKNOWN);
 
+    private VerbType associatedVerbType;
+    ClauseType(VerbType verbType) {
+        associatedVerbType = verbType;
+    }
+
+    public VerbType getVerbType() {
+        return associatedVerbType;
+    }
 }
---
+++ src/main/java/com/mathproblems/solver/VerbType.java
@@ -0,0 +1,12 @@
+package com.mathproblems.solver;
+
+public enum VerbType {
+
+    COMPLEX_TRANSITIVE,
+    COPULAR,
+    DITRANSITIVE,
+    EXTENDED_COPULAR,
+    INTRANSITIVE,
+    MONOTRANSITIVE,
+    UNKNOWN;
+}
